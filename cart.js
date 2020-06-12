0eaea0298ecdcd8d6dbeaaa822fff738504e1b4c
shadowstik <dao.cam41@gmail.com>
2020-06-11 23:04:26 -0400

    fonction ajout au panier

--- cart.js
+++ cart.js
@@ -1,12 +1,49 @@
-class AddCart {
-    constructor (id, name, price, description, imageUrl) {
-        this.productId = id;
+
+
+class RowCart {
+    constructor (name, price, imageUrl, qte) {
         this.productName = name;
         this.productPrice = price;
-        this.productDescription = description;
         this.productImageUrl = imageUrl;
+        this.productQte = qte;
+        this.addQte = function(qte) {
+            this.productQte += qte;
+        }
+        this.getRowPrice = function() {
+            let result = this.productPrice * this.productQte;
+            return result;
+        }
+        this.getName = function() {
+            return this.productName;
+        }
+
     }
 };
+
+function cart () {
+    this.liste = [];
+    this.addProduct = function(name, price, imageUrl, qte) { 
+        let index = this.getProduct(name);
+        if (index == -1) this.liste.push(new RowCart(name, price, imageUrl, qte));
+        else this.liste[index].addQte(qte);
+    }
+    this.getPriceCart = function() {
+        let total = 0;
+        for(let i = 0 ; i < this.liste.length ; i++)
+            total += this.liste[i].getRowPrice();
+        return total;
+    }
+    this.getProduct = function(name) {
+        for(var i = 0 ; i <this.liste.length ; i++)
+            if (name == this.liste[i].getName()) return i;
+        return -1;
+    }
+    this.removeProduct = function(name)
+    {
+        let index = this.getProdcut(name);
+        if (index > -1) this.liste.splice(index, 1);
+    }
+}
 
 const request = new XMLHttpRequest();
 request.onreadystatechange = function() {
@@ -14,6 +51,5 @@ request.onreadystatechange = function() {
         let response = JSON.parse(this.responseText);
         console.log(response.lenses.name);
 };
-
 request.open("GET", "http://localhost:3000/api/cameras");
 request.send();
